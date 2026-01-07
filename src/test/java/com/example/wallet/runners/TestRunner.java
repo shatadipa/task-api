7bebcd1342bc78fc6cec40feb953076e0ab15522
Shatadipa <shatadipa1986@gmail.com>
2026-01-08 01:14:45 +0530

    Adds the Cucumber Test Runner which is the entry point for the tests

---
+++ src/test/java/com/example/wallet/runners/TestRunner.java
@@ -0,0 +1,17 @@
+package com.example.wallet.runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"com.example.wallet.steps"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "json:target/cucumber-report.json"
+        },
+        monochrome = true
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+}
